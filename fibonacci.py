b6bfee52e623d8a66863684bbb9d93af30a49511
Damephena <34281511+Damephena@users.noreply.github.com>
2020-05-26 16:17:23 +0100

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,11 @@
 def fib(number):
-    if number <= 0:
-        print('Please enter a positive integer.')
-    elif number == 1:
+    if number <= 1:
         return 0
     elif number == 2:
         return 1
     else:
         return fib(number - 1) + fib(number - 2)
 
-number = int(input('Enter a valid number: \n'))
+number = int(input('Enter a valid positive number: \n'))
 
 print("Fibonacci value is: ", fib(number))
